a78a7ce77cd7ae73d73a4bad7f6e9ae25937d5e1
Carl O'Beirne <carlobeirne98@gmail.com>
2020-02-21 15:24:43 +0000

    Initial Commit

---
+++ Loan_Data_Analysis.R
@@ -0,0 +1,6 @@
+# Title: Loan Data Analysis
+# Contributors: Carl OBeirne | Jamie Hackett | Marlon Da Hora
+# Student IDs: X16326186 | X16386576 | X16117492
+# Module: Data & Web Mining (CA2)
+# Submitted to: Mr. Noel Cosgrave
+# Due Date: April 5th 2020
