b1d9c1f1712e5b716ef12f89805ece39196efd23
Carl O'Beirne <carlobeirne98@gmail.com>
2020-03-26 16:17:13 +0000

    Removed DB Connection. Set the seed for reproducibility. Finalized random forest model. Final commit

--- Carl_Loan_Data_Analysis.R
+++ Carl_Loan_Data_Analysis.R
@@ -1,24 +1,4 @@
 # Workbench for Carl O'Beirne's section of the project
-library(RMariaDB)
-library(bigmemory)
-
-# Establishing a connection to the Database
-rmariadb.settingsfile <- "C:/temp/loan-data-analysis.cnf"
-rmariadb.db <- "loan-data-analysis"
-loanAnalysisDB <- dbConnect(RMariaDB::MariaDB(), default.file=rmariadb.settingsfile, group=rmariadb.db)
-
-# Database Query to Select and Store all of the Rows for data cleansing
-loan_data <- dbSendQuery(loanAnalysisDB, "SELECT * FROM loan_data")
-loan_data <- dbFetch(loan_data)
-
-# Adding NA where there is a blank in the Data
-loan_data[loan_data == ''] <- NA 
-
-# Counting NA Values
-sapply(loan_data, function(x) sum(is.na(x))) 
-
-# Removing rows that has NA in Emp_Title
-loan_data <- loan_data[!is.na(loan_data$emp_title), ] 
 
 # Install Packages
 install.packages(c("randomForest", "caret"))
@@ -26,33 +6,32 @@ install.packages(c("randomForest", "caret"))
 # Reference Libraries
 library(randomForest)
 library(caret)
-library(data.table)
+# library(data.table)
 
 # Saving the cleaned data to CSV to be reuploaded to the database
 #write.csv(loan_data, "CleanedLoanData.csv") 
-
-loan_data <- fread("CleanedLoanData.csv",stringsAsFactors = TRUE)
+loan_data_raw <- read.csv("CleanedLoanData.csv",stringsAsFactors = TRUE)
 
 # Beginning Analysis
 
-# Making Dataset for Carl's Analysis (Removing Column 1 & 9)
-loan_data_carl <- loan_data[,-c(9,24)]
+# Setting the seed for reproucable results
+set.seed(16326186)
 
-str(loan_data_carl)
+str(loan_data_raw$emp_title)
+
+
+# Making Dataset for Carl's Analysis
+loan_data_carl <- loan_data_raw[,-c(5,8,9,24)] #Removing Sub-Grade RF Model
 
 sapply(loan_data_carl, function(x) sum(is.na(x))) #Check for nulls
 
-# loan_data$annual_inc <- as.integer(loan_data$annual_inc)
-# loan_data$out_prncp <- as.integer(loan_data$out_prncp)
-# 
-# 
-# loan_data$home_ownership <- as.factor(loan_data$home_ownership)
-# loan_data$emp_length <- as.factor(loan_data$emp_length)
-
+# Setting the seed for reproucable results
+set.seed(16326186)
 # Creating a sample
-index <- sample(1:nrow(loan_data_carl), 0.1*nrow(loan_data_carl), replace = F)
+index <- sample(1:nrow(loan_data_carl), 0.05*nrow(loan_data_carl), replace = F)
 loanSample <- loan_data_carl[index, ]
-
+# Setting the seed for reproucable results
+set.seed(16326186)
 # Creating train and test data from the sample
 index <- sample(1:nrow(loanSample), 0.75*nrow(loanSample) , replace = F)
 loanTrain <- loanSample[index, ] 
@@ -60,30 +39,20 @@ loanTest <- loanSample[-index, ]
 
 rm(index) # Removing index - No longer required
 
-# loan_data_train$int_rate <- as.integer(loan_data_train$int_rate)
-# loan_data_test$int_rate <- as.integer(loan_data_test$int_rate)
-# 
-# loan_data_train$purpose <- as.factor(loan_data_train$purpose)
-# loan_data_test$purpose <- as.factor(loan_data_test$purpose)
+# Model 1 to predict Grade (Without Sub Grade)
+rf_model <- randomForest(grade ~., loanTrain)
 
-# Model 1 to predict Home Ownership
-rf_model <- randomForest(home_ownership ~., loanTrain)
-varImpPlot(rf_model , col = "purple", bg = "colour")
+# Variable Importance Plot to identify what variables are most important in the model
+varImpPlot(rf_model, main = "Variable Importance Plot - Loan Grade", col = "red", bg = "red")
 
+#Predicting the results
 predRF <- predict(rf_model, loanTest)
-HomeOwnCM <- confusionMatrix(predRF, loanTest$home_ownership)
+gradeCM <- confusionMatrix(predRF, loanTest$grade)
+gradeCM
 
-#RF Model 2
-rf_model2 <- randomForest(sub_grade ~., loanTrain)
+# Check how many of each variable are used in the model
+varUsed(rf_model)
 
-varImpPlot(rf_model2) 
-
-predRF2 <- predict(rf_model2, newdata = loan_data_test, type = "class")
-PurposeCF <- confusionMatrix(predRF2, loan_data_test[,15])
-
-table(loan_data$home_ownership)
-table(loan_data_test$home_ownership)
-table(loan_data_train$home_ownership)
-
-
-str(loan_data_train$purpose)
+# Table to see correct distribution of grades
+table(loanTest$grade)
+table(loan_data_raw$sub_grade)
